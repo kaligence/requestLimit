6327f973e8a9f88b4e5074baa208d9008fcafe0c
kaligence <hbht0123456789@163.com>
2018-01-05 16:47:31 +0800

    更换包名

--- src/main/java/com/github/kaligence/requestLimit/RequestLimit.java
+++ src/main/java/com/github/kaligence/requestLimit/RequestLimit.java
@@ -1,8 +1,7 @@
-package com.ice.rql;
+package com.github.kaligence.requestLimit;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-
 import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
@@ -11,12 +10,13 @@ import java.lang.annotation.*;
 //最高优先级
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public @interface RequestLimit {
-	/*
-	 * 请求次数限制
-	 */
-	long requestCount() default 1L;
-	/*
-	 * 请求限制单位时间 /s
-	 */
-	long requestTime() default 60L;
+    /*
+     * 请求次数限制
+     */
+    long requestCount() default 1L;
+
+    /*
+     * 请求限制单位时间 /s
+     */
+    long requestTime() default 60L;
 }
